55fc4618451f45d8ed8041de560d08e351a53b41
Richi Fendrika <ASUS@LAPTOP-18FMB87U>
2022-11-07 16:52:16 +0700

    menambahkan class

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -6,13 +6,25 @@ using System.Threading.Tasks;
 
 namespace singly_linked_list
 {
+    //each node consist of the information part and link to the next node
     class node
     {
-
+        public int rollnumber;
+        public string name;
+        public node next;
     }
     class list
     {
+        node START;
 
+        public list()
+        {
+            START - null;
+        }
+        public void addNote()
+        {
+
+        }
     }
     class Program
     {
