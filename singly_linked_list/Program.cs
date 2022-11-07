a352f78214df4033475365151e3c63b564ccb654
Richi Fendrika <ASUS@LAPTOP-18FMB87U>
2022-11-07 16:48:16 +0700

    Add project files.

---
+++ singly_linked_list/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singly_linked_list
+{
+    class node
+    {
+
+    }
+    class list
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
